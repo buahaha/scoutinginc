ce14d804dac12f2510e1507dd1730ba8f7c49fbe
Szymon Błaszczyński <museyoucoulduse@gmail.com>
2020-07-11 21:47:34 +0200

    inGameDescriptions made medium
    !important

--- scoutinginc/src/App.js
+++ scoutinginc/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Container, Jumbotron, Row, Image, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
 // var HtmlToReactParser = require('html-to-react').Parser;
 import {Parser} from 'html-to-react';
-import HtmlToReact from 'html-to-react';
+// import HtmlToReact from 'html-to-react';
 
 import TweetEmbed from 'react-tweet-embed';
 
@@ -51,19 +52,6 @@ class App extends React.Component {
     
   }
 
-  // async getProfile() {
-  //   await axios.get('/profile/xxx')
-  //     .then((response) => {
-
-  //     })
-  //       .catch((error) => {
-
-  //       })
-  //         .finally(() => {
-  //           console.log('bow to the Emperor')
-  //         })
-  // }
-
   componentDidMount() {
     this.pronuncedAtCourt();
   }
@@ -72,64 +60,96 @@ class App extends React.Component {
     const temperature = this.state.callSign;
     const corp_desc = this.state.corp_description;
     const ceo_desc = this.state.ceo_description;
-    const ceo = this.state.ceo;const date_founded = new Date(temperature.date_founded).toLocaleString();
+    // const ceo_desc = 
+    const ceo = this.state.ceo;
+    const date_founded = new Date(temperature.date_founded).toLocaleString();
     return (
       <Container className="App" bg='light'>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Welcome to <code>Scouting Inc</code> corporation.
-          </p>
-          <p className='inGameDescription' fontSize="8"> 
-            { corp_desc}
-          </p>
-        </header>
         <Container>
+          <header className="App-header">
+            {/* <Jumbotron> */}
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Welcome to <code>Scouting Inc</code> corporation.
+              </p>
+              <h1 className='corpDescription'> 
+                { corp_desc}
+              </h1>
+            {/* </Jumbotron> */}
+          </header>  
+        </Container>
+        <Container id="content">
 
           <article>
-
-            <Container id="tweet">
-              <TweetEmbed id="1279402833310175232" />
+            <Container fluid>
+              <Row>
+                <Row>
+                  <Col>
+                  
+                    <Container id="tweet">
+                      <TweetEmbed id="1279402833310175232" />
+                    </Container>
+                  </Col>
+                  <Col>
+                    <p >
+                      { temperature.member_count} players under the rule of { ceo.name } commonly 
+                      pronounced as the CEO, since { date_founded } flying under 
+                      ticker [{ temperature.ticker }] and sharing { temperature.tax_rate * 100 }% tax.
+                    </p>
+                    <p className='inGameDescription'>
+                      { ceo_desc }
+                    </p>
+                  </Col>
+                </Row>
+                <Row>
+                
+                </Row>
+              </Row>
             </Container>
-            <p >
-              { temperature.member_count} players under the rule of { ceo.name } commonly 
-              pronounced as the CEO, since { date_founded } flying under 
-              ticker [{ temperature.ticker }] and sharing { temperature.tax_rate * 100 }% tax.
-            </p>
-            <h3 className="inGameDescription" fontSize="8">
-              { ceo.name }'s credo:<br />
-              <cite>
-                {ceo_desc}
-              </cite>
-            </h3>
-            <a
-              className="App-link"
-              href="https://toscn.herokuapp.com"
-              target="_toscn"
-              rel="noopener noreferrer"
-            >
-              TOSCN 😇 friendly industrialists and explorers
-            </a>
-            <a
-              href="https://neosb.herokuapp.com"
-              target="_neosb"
-
-            >
-              Neosb 🎶 the aspiring electronic musician 
-            </a>
-            <a
-              href="https://wiki.eveuniversity.org/"
-              target="_uniwiki"
-            >
-              UNIWIKI 📗
-            </a>
-            <a
-              href="https://support.eveonline.com/hc/en-us"
-              target="_support"
-            >
-              EVE Online 🚑 Support 
-            </a>
           </article>
+            <Container fluid>
+              <Row>
+                <Col>
+                
+                  <p>
+
+                    <a
+                      className="App-link"
+                      href="https://toscn.herokuapp.com"
+                      target="_toscn"
+                      rel="noopener noreferrer"
+                      >
+                      TOSCN 😇 friendly industrialists and explorers
+                    </a>
+                    <br />
+                    <a
+                      className="App-link"
+                      href="https://neosb.herokuapp.com"
+                      target="_neosb"
+                      
+                      >
+                      Neosb 🎶 the aspiring electronic musician 
+                    </a>
+                    <br />
+                    <a
+                      className="App-link"
+                      href="https://wiki.eveuniversity.org/"
+                      target="_uniwiki"
+                      >
+                      UNIWIKI 📗
+                    </a>
+                    <br />
+                    <a
+                      className="App-link"
+                      href="https://support.eveonline.com/hc/en-us"
+                      target="_support"
+                      >
+                      EVE Online 🚑 Support 
+                    </a>
+                  </p>
+                </Col>
+              </Row>
+            </Container>
         </Container>
         <footer>
 
